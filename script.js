a600706d2b3fd6fe082300b293dc315075906750
= <48904692+nhuddse02393@users.noreply.github.com>
2019-04-05 09:00:35 +0700

    add project

---
+++ script.js
@@ -0,0 +1,17 @@
+let Circle = function(radius) {
+    this.radius = radius;
+
+    this.getRadius = function() {
+        return this.radius;
+    }
+
+    this.getArea = function() {
+        return Math.PI * this.radius * this.radius;
+    }
+};
+
+let circle = new Circle(5);
+let radius = circle.getRadius();
+let area = circle.getArea();
+
+alert("radius: " + radius + "; area: " + area);
